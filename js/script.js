6f8a3c758753208db62db2138c04a035d8ec9735
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2021-11-09 17:07:01 +0100

    back save
    back save

--- js/script.js
+++ js/script.js
@@ -1,34 +1,15 @@
-$(document).ready(function () {
-  $(".slider__item").each(function (key, item) {
-    console.log($(item).attr("data-name"));
-    const name = $(item).attr("data-name") || null;
-    if (name != null) {
-      $(".slider-nav").append(`
-           <a data-index="${key}" href="#" class="slider-nav-link">
-               ${name}
-           </a>`);
-    }
-  });
-
-  const slider = $(".slider").slick({
-    // dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: "linear",
-    prevArrow: ".prev-arrow",
-    nextArrow: ".prev-arrow",
-  });
-
-  $(document).on("click", ".slider-nav-link", function (e) {
-    console.log("asdasd");
-
-    e.preventDefault();
-    const index = +$(this).attr("data-index");
-    slider.slick("slickGoTo", index);
-  });
+let flag = false;
+$(".header-menu-toggle").click(function () {
+  console.log("zxc");
+  if (!flag) {
+    $(".header-menu").slideDown(200);
+  } else {
+    $(".header-menu").slideUp(200);
+  }
+  flag = !flag;
 });
 
+// Drop textarea function
 function textArea() {
   document.getElementById("textarea").style.display = "initial";
 }
