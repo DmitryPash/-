12cc6a5cf541c4991126f6280a12bcc1e7faae3e
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2021-11-10 13:08:01 +0100

    Slider
    add Slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// drop Menu - Burger
 let flag = false;
 $(".header-menu-toggle").click(function () {
   console.log("zxc");
@@ -13,3 +14,36 @@ $(".header-menu-toggle").click(function () {
 function textArea() {
   document.getElementById("textarea").style.display = "initial";
 }
+// Slick slider
+$(document).ready(function () {
+  $(".slider__item").each(function (key, item) {
+    console.log($(item).attr("data-name"));
+    const name = $(item).attr("data-name") || null;
+    if (name != null) {
+      $(".slider-nav").append(`
+          <a data-index="${key}" href="#" class="slider-nav-link">
+              ${name}
+          </a>`);
+    }
+  });
+
+  const slider = $(".slider").slick({
+    // dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+    prevArrow: ".prev-arrow",
+    nextArrow: ".prev-arrow",
+  });
+
+  $(document).on("click", ".slider-nav-link", function (e) {
+    console.log("asdasd");
+
+    e.preventDefault();
+    const index = +$(this).attr("data-index");
+    slider.slick("slickGoTo", index);
+  });
+});
+
+// $(document).ready(function () {});
